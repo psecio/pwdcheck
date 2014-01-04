4c22af3e50c6dcbc4f79b45ccf2b28ea3524a1e2
enygma <enygma@phpdeveloper.org>
2014-01-04 07:27:22 -0600

    adding docblock for abstract evaluate method in check class

--- src/Psecio/Pwdcheck/Check.php
+++ src/Psecio/Pwdcheck/Check.php
@@ -4,5 +4,12 @@ namespace Psecio\Pwdcheck;
 
 abstract class Check
 {
+	/**
+	 * Required evaluation function - runs the
+	 * 	checking for the rule
+	 * 
+	 * @param array $passwordData Parsed password data
+	 * @return integer Resulting score
+	 */
     public abstract function evaluate($passwordData);
 }
