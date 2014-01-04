a4220d645d2ea76454496092865860d731797bdd
enygma <enygma@phpdeveloper.org>
2014-01-04 07:11:03 -0600

    Updating Password class to get strength data and add docblocks

--- src/Psecio/Pwdcheck/Password.php
+++ src/Psecio/Pwdcheck/Password.php
@@ -4,7 +4,21 @@ namespace Psecio\Pwdcheck;
 
 class Password
 {
+    /**
+     * Current password's total score
+     * @var integer
+     */
     private $score = 0;
+
+    /**
+     * Constants defining the return values for 
+     * strength ratings
+     */
+    const STRENGTH_VERY_WEAK = 0;
+    const STRENGTH_WEAK = 1;
+    const STRENGTH_FAIR = 2;
+    const STRENGTH_STRONG = 3;
+    const STRENGTH_VERY_STRONG = 4;
 
     /**
      * List of tests in the order to process
@@ -19,6 +33,36 @@ class Password
         'seqNumber', 'seqCharacter', 'extra'
     );
 
+    /**
+     * Get the current password strength
+     *     Either numeric or as plain text
+     * 
+     * @param boolean $asText Flag to return either integer or text
+     * @return mixed Either integer or string repreesnting strength
+     */
+    public function getStrength($asText = false)
+    {
+        $score = $this->getScore();
+
+        if ($score <= 60) {
+            return ($asText) ? 'Very Weak' : self::STRENGTH_VERY_WEAK;
+        } elseif ($score > 60 && $score <= 70) {
+            return ($asText) ? 'Weak' : self::STRENGTH_WEAK;
+        } elseif ($score > 70 && $score <= 80) {
+            return ($asText) ? 'Fair' : self::STRENGTH_FAIR;
+        } elseif ($score > 80 && $score <= 90) {
+            return ($asText) ? 'Strong' : self::STRENGTH_STRONG;
+        } else {
+            return ($asText) ? 'Very Strong' : self::STRENGTH_VERY_STRONG;
+        }
+    }
+
+    /**
+     * Evaluate the given password string
+     * 
+     * @param string $password Password to evaluate
+     * @return integer Resulting score
+     */
     public function evaluate($password)
     {
         $passwordData = $this->parse($password);
@@ -29,17 +73,19 @@ class Password
             if (class_exists($className)) {
                 $test = new $className();
                 $result = $test->evaluate($passwordData);
-                echo $testName.' - '.$result."\n";
-
                 $score += $result;
             }
         }
-
-        echo "\n\n".'SCORE: '.$score."\n";
-
-        return $this->getScore();
+        $this->setScore($score);
+        return $score;
     }
 
+    /**
+     * Parse the password into relevant data
+     * 
+     * @param string $password Password to evaluate
+     * @return array Parsed password data
+     */
     public function parse($password)
     {
         $passwordData = array(
@@ -85,10 +131,21 @@ class Password
         return $passwordData;
     }
 
+    /**
+     * Get the current score
+     * 
+     * @return integer Score value
+     */
     public function getScore()
     {
         return $this->score;
     }
+
+    /**
+     * Set the current score
+     * 
+     * @param integer $score
+     */
     public function setScore($score)
     {
         $this->score = $score;
